962e061f12df45c8b78803241de88e34ebd06d3f
Kushagra <84122166+Kushagra301@users.noreply.github.com>
2021-11-16 23:37:41 +0530

    all the files are added

---
+++ client/src/components/chat/dummyUsers.js
@@ -0,0 +1,37 @@
+export const dummyUsers = [{
+        id: "2",
+        name: "Krishna Harchandani",
+        email: "krishna@gmail.com",
+        photoUrl: "https://randomuser.me/api/portraits/men/1.jpg",
+        role: "Developer",
+        info: "Product Designer at Vit Vellore",
+        welcomeMessage: "Hey there! Come to me to clear you HTML doubts :-)"
+    },
+    {
+        id: "3",
+        name: "Kushagra Gupta",
+        email: "kushagra@gmail.com",
+        photoUrl: "https://randomuser.me/api/portraits/men/4.jpg",
+        role: "Developer",
+        info: "Developer at Vit Vellore",
+        welcomeMessage: "Hey there! Come to me for your CSS doubts :-)"
+    },
+    {
+        id: "4",
+        name: "Aryan Jain",
+        email: "Aryanjain@gmail.com",
+        photoUrl: "https://randomuser.me/api/portraits/women/44.jpg",
+        role: "Developer",
+        info: "Product Designer at Vit Vellore",
+        welcomeMessage: "Hey there! Come to me for your JavaScript doubts :-)"
+    },
+    {
+        id: "5",
+        name: "Loperm Ipsum",
+        email: "lorem@sample.com",
+        photoUrl: "https://pbs.twimg.com/profile_images/834493671785525249/XdLjsJX_.jpg",
+        role: "Member",
+        info: "Software Engineer at TalkJS",
+        welcomeMessage: "Hey there! Love to chat :-)"
+    },
+]
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://IWP:1234@cluster0-shard-00-00.wpm6r.mongodb.net:27017,cluster0-shard-00-01.wpm6r.mongodb.net:27017,cluster0-shard-00-02.wpm6r.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-2vx256-shard-0&authSource=admin&retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+};
